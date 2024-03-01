f610a9f306c46df7a14c5bae0b6b2e107423bfa3
Abdellah El Halimi <abdellah.elhalimimerroun@student.hogent.be>
2024-03-01 10:03:24 +0100

    errors bij main

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,37 +4,66 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import persistentie.Connectie;
 
-public class Main extends Application
+//public class Main extends Application
+//
+//{
+//	public static void main(String[] args) {
+//		if (args.length == 1) {
+//			Connectie.setSshPrivateKeyPath(args[0]);
+//		}
+//		launch(args);
+//	}
+//
+//	@Override
+//	public void start(Stage primaryStage) throws Exception {
+//
+////            try {
+////                Connection connection = DatabaseConnection.getConnection();
+////                System.out.println("Connected to database!");
+////                connection.close();
+////            } catch (SQLException e) {
+////                System.out.println("Failed to connect to database: " + e.getMessage());
+////            }
+//
+//		Parent root = FXMLLoader.load(getClass().getResource("/gui/welkomKD.FXML"));
+//		Scene myScene = new Scene(root);
+//		primaryStage.setScene(myScene);
+//		primaryStage.setTitle("Kingdomino Registratie");
+//		primaryStage.show();
+//		primaryStage.setResizable(false);
+//
+//	}
+//}
 
-{
+public class Main extends Application {
+
+	private Stage primaryStage;
+	private Scene scene;
+	private VBox vbox;
+
 	public static void main(String[] args) {
-		if (args.length == 1) {
-			Connectie.setSshPrivateKeyPath(args[0]);
-		}
 		launch(args);
 	}
 
 	@Override
-	public void start(Stage primaryStage) throws Exception {
+	public void start(Stage primaryStage) {
+		this.primaryStage = primaryStage;
+		try {
+			// Laad het FXML-bestand
+			Parent root = FXMLLoader.load(getClass().getResource("/gui/welkomKD.FXML"));
 
-//            try {
-//                Connection connection = DatabaseConnection.getConnection();
-//                System.out.println("Connected to database!");
-//                connection.close();
-//            } catch (SQLException e) {
-//                System.out.println("Failed to connect to database: " + e.getMessage());
-//            }
+			// Creëer de scene
+			Scene scene = new Scene(root, 1600, 1600);
 
-//    	welkomController root2 = new welkomController();
-		Parent root = FXMLLoader.load(getClass().getResource("/gui/welkom.FXML"));
-		Scene myScene = new Scene(root);
-		primaryStage.setScene(myScene);
-		primaryStage.setTitle("Kingdomino Registratie");
-		primaryStage.show();
-		primaryStage.setResizable(false);
-
+			// Stel het podium in
+			primaryStage.setTitle("Mijn JavaFX App");
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
