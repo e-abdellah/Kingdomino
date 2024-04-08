a72f67c106a296aed92b3f06d9e38839e7b0ef86
AndresDeConinck <andres.deconinck@student.hogent.be>
2024-04-08 17:35:11 +0200

    Aanpassingen

--- src/module-info.java
+++ src/module-info.java
@@ -21,5 +21,6 @@ module Kingdomino_g36 {
 	requires org.junit.jupiter.api;
 	requires org.junit.jupiter.params;
 	requires org.junit.platform.commons;
+	requires jsch;
 	opens gui to javafx.fxml;
 }
